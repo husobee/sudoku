f61c2ef9fb916b444f87447fb88a0009a9047b58
Ben Huson <husobee@e-ie.io>
2015-11-22 10:18:55 -0500

    adding comments around utils

--- utils.go
+++ utils.go
@@ -3,23 +3,29 @@ package sudoku
 import "bufio"
 
 const (
-	underscore        byte = 0x5f
-	space                  = 0x20
-	zero                   = 0x30
-	one                    = 0x31
-	nine                   = 0x39
-	newline                = 0x0a
-	maxInputRowLength int  = 17
+	// byte values for the allowed tokens, for use with custom
+	// scanner split function
+	underscore byte = 0x5f
+	space           = 0x20
+	zero            = 0x30
+	one             = 0x31
+	nine            = 0x39
+	newline         = 0x0a
+	// this is the maximum length of a row in bytes
+	maxInputRowLength int = 17
 )
 
+// isSpace - validate this char is a space
 func isSpace(c byte) bool {
 	return c == space
 }
 
+// isBlank - validate this char is an underscore
 func isBlank(c byte) bool {
 	return c == underscore
 }
 
+// isNumber - validate this char is a number
 func isNumber(c byte) bool {
 	if _, err := asciiToNumber(c); err != nil {
 		return false
@@ -27,6 +33,7 @@ func isNumber(c byte) bool {
 	return true
 }
 
+// asciiToNumber - convert from ascii representation to uint8
 func asciiToNumber(c byte) (uint8, error) {
 	if c < one || c > nine {
 		return uint8(c), ErrParseInvalidNumber
@@ -34,11 +41,15 @@ func asciiToNumber(c byte) (uint8, error) {
 	return uint8(c) - zero, nil
 }
 
+// isEvenNumber - validate this is an even number
 func isEvenNumber(i int) bool {
 	return i%2 == 0
 }
 
+// puzzleScanSplit - This is the customer scanner split function used to both
+// parse and validate the stdin representation of the puzzle
 func puzzleScanSplit(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	// based on scanlines, we will validate each line at a time
 	advance, token, err = bufio.ScanLines(data, atEOF)
 	if err == nil && token != nil {
 		if len(token) != maxInputRowLength {
